f524108ede95578445a6eee060092518e1510ea9
André LS Meirelles <alsmeirelles@gmail.com>
2022-04-17 12:33:42 -0400

    Merge

--- batch_jobs/inception.sh
+++ batch_jobs/inception.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -p GPU-shared
 #SBATCH -N 1
-#SBATCH -t 30:00:00
-#SBATCH --gpus=v100-32:2
+#SBATCH -t 5:00:00
+#SBATCH --gpus=v100-32:1
 #SBATCH --exclude=v034
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=alsmeirelles@gmail.com
@@ -10,7 +10,7 @@
 #echo commands to stdout
 #set -x
 
-DIRID="DB/DB-43"
+DIRID="DB/DB-50"
 cd /ocean/projects/asc130006p/alsm/active-learning/Segframe
 
 echo '[VIRTUALENV]'
@@ -23,7 +23,7 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/ocean/projects/asc130006p/alsm/venv/lib
 echo '[START] training'
 date +"%D %T"
 
-time python3 main.py -i -v --train --pred -predst /ocean/projects/asc130006p/alsm/active-learning/data/AL-214-Aug -split 0.9 0.005 0.0 -net EFInception -data AqSet -d -k -e 50 -b 128 -tdim 240 240 -f1 10 -out logs/ -cpu 15 -gpu 2 -tn -sv -nsw -wpath results/$DIRID -model_dir results/$DIRID -logdir results/$DIRID -cache results/$DIRID -test_dir /ocean/projects/asc130006p/alsm/active-learning/data/T5-2-test -phi 1 
+time python3 main.py -i -v --train --pred -predst /ocean/projects/asc130006p/alsm/active-learning/data/AL-367 -split 0.99 0.01 0.0 -net EFInception -data AqSet -d -k -e 50 -b 96 -tdim 240 240 -f1 10 -out logs/ -cpu 15 -gpu 1 -sv -nsw -wpath results/$DIRID -model_dir results/$DIRID -logdir results/$DIRID -cache results/$DIRID -test_dir /ocean/projects/asc130006p/alsm/active-learning/data/Test-T8 -phi 1 -lr 0.0005 -tn -aug 
 
 
 echo '[FINAL] done training'
